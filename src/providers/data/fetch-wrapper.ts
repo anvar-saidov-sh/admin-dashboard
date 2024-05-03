3af70e7816e0be28d9991be704f0b8d964fe6281
anvarsaidovsh <bananoanimation12@gmail.com>
2024-05-03 16:54:32 +0500

    If-Else conditions

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,4 +1,10 @@
+
 import {GraphQLFormattedError} from "graphql";
+
+type Error = {
+    message: string;
+    statusCode: string;
+}
 
 const  customFetch = async (url: string, options: RequestInit) =>{
     const accessToken = localStorage.getItem('access_token');
@@ -21,4 +27,17 @@ const getGraphQLErrors = (body: Record<"errors",GraphQLFormattedError[] | undefi
             statusCode: "INTERNAL_SERVER_ERROR"
         }
     }
+    if ("errors" in body) {
+        const errors = body?.errors;
+
+        const messages = errors?.map((error) => error?.message)?.join("");
+        const code = errors?.[0]?.extensions?.code
+
+        return {
+            message: messages || JSON.stringify(errors),
+            statusCode: code || 500,
+
+        }
+    }
+    return null;
 }
