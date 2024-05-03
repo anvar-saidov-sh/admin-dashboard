6b22d7e27b57a2f319dc81b924fc02c91d33ddec
anvarsaidovsh <bananoanimation12@gmail.com>
2024-05-03 16:47:18 +0500

    Adding data

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -3,5 +3,11 @@ import { GraphQLClient } from "@refinedev/nestjs-query";
 export const API_URL = 'https://api.crm.refine.dev'
 
 export const client = new GraphQLClient(API_URL, {
-    fetch:
+    fetch: (url: string, options: RequestInit)=>{
+        try {
+            return fetchWrapper(url, options)
+        } catch (error) {
+            return Promise.reject(error as Error);
+        }
+    }
 })
