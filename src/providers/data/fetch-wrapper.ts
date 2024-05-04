d1d8914e0f464c85ebbe3f4efd23de3edb75319d
anvarsaidovsh <bananoanimation12@gmail.com>
2024-05-04 20:38:35 +0500

    Unknown issue@ @

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -20,7 +20,7 @@ const  customFetch = async (url: string, options: RequestInit) =>{
         }
     })
 }
-const getGraphQLErrors = (body: Record<"errors",GraphQLFormattedError[] | undefined>) : Error | null =>{
+const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>) : Error | null =>{
     if (!body) {
         return {
             message: 'Unknown error',
